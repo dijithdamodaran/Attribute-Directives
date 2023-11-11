cd3d88d4a13232c0c3e3785fb1d95ec71a7858e1
dijithdamodaran <dijithdamodaran@gmail.com>
2023-11-11 10:52:56 +0530

    attribute-directives-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'],
+styles:[`
+.applycolor{
+background-color:yellow;
+color:blue
+  }
+`]
+ 
 })
 export class AppComponent {
   title = 'attribute-directives-demo';
+  colorApplicatiuon:boolean=true;
 }
